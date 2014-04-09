06954d1fd2291585446f126101a4aed196ef63f0
marcos.ocf01 <marcos.ocf01@gmail.com>
2014-04-09 16:47:42 -0300

    otimizacao do terminal e fim dos testes

--- lib/calculadora/command.rb
+++ lib/calculadora/command.rb
@@ -1,19 +1,35 @@
 module Calculadora
 	class Command
-		def initialize str
-			@c = Calc.new
-			self.enviar_calc(str)
+		def initialize str = "help"
+			array_char = str.chars
+			array_char.delete(nil)
+			array_char.delete(" ")
+			array_char.delete("")
+
+			if array_char != ["h","e","l","p"]
+				@c = Calc.new
+				self.enviar_calc!(array_char)
+			else
+				@help = true
+			end
+
+		end
+
+		def texto_help
+		  "\tDigite a expressão desejada entre aspas \n \tutilizando os operadoes +, -, *, / \n para obter a expressão formatada com resultado"
 		end
 
 		def formatar
-			@c.formatar
+			@help ? texto_help : @c.formatar
 		end
 
-		def enviar_calc  str
-			while str != ""
-				@c.send(str[0].to_sym,str[1].to_f)
-				str = str.slice(2..-1)
-				
+		def enviar_calc! array
+			array.unshift(:+) if array.length % 2 != 0
+			while array[0] != nil
+				@c.send(array[0], array[1].to_f)
+				array.shift
+				array.shift
+
 			end
 		end 
 	end
--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -3,11 +3,31 @@ require_relative "../lib/calculadora/Command.rb"
 
 describe Calculadora::Command do 
 	
-	describe "Inicilializacao" do
-		it "Deveria retornar a expressao formatada com resultado" do
-			array = "+  1 -2 + 2*3-2"
-			array.delete!(" ")
-			Calculadora::Command.new(array).formatar.should eql "0.0  +  1.0  -  2.0  +  (  2.0  *  3.0  )  -  2.0  =  3.000"
+	describe "inicilializacao" do
+		context "quando não informar o operador inicial" do
+			it "deveria retornar a expressao formatada com resultado" do
+				str = "1-2+2*3-2"
+				Calculadora::Command.new(str).formatar.should eql "0.0  +  1.0  -  2.0  +  (  2.0  *  3.0  )  -  2.0  =  3.000"
+			end
+		end
+
+		context "quando informar o operador inicial" do
+			it "deveria retornar a expressao formatada com resultado" do
+				array = "-1-2+2*3-2"
+				Calculadora::Command.new(array).formatar.should eql "0.0  -  1.0  -  2.0  +  (  2.0  *  3.0  )  -  2.0  =  1.000"
+			end
+		end
+
+		context "quando não informar nada" do
+			it "deveria exibir o help" do
+				Calculadora::Command.new.formatar.should eql "\tDigite a expressão desejada entre aspas \n \tutilizando os operadoes +, -, *, / \n para obter a expressão formatada com resultado"
+			end
+		end
+		context "quando os operadores estiverem separados dos numeros" do
+			it "deveria eliminar o espacos em branco e operar normalmente" do
+				array = "-1 - 2+2 *3- 2"
+				Calculadora::Command.new(array).formatar.should eql "0.0  -  1.0  -  2.0  +  (  2.0  *  3.0  )  -  2.0  =  1.000"
+			end
 		end
 	end
 end
